a5278d4dd793f7ae60125c2929d72b283270a5ff
Chris Barts <chbarts@gmail.com>
2022-09-23 21:21:49 -0600

    Change the styles

--- rss-to-html.py
+++ rss-to-html.py
@@ -16,30 +16,30 @@ def make_html(feed):
         with air.head():
             air.meta(charset="utf-8")
             air.title(_t=feed.title)
-        with air.body(style="max-width: 1080 px; margin: 0 auto !important; float: none !important;"):
-            with air.div(klass="description", style="background-color: rgba(255, 255, 255, .5);"):
-                with air.p():
+        with air.body():
+            with air.div(klass="main"):
+                with air.p(klass="description", style="background-color: rgb(220,220,220); margin: 1em;"):
                     air.strong(_t="Description: ")
                     air(feed.description)
-            air.h1(_t="Items")
-            for item in feed.items:
-                with air.div(klass="item", style="border: 1px solid black;"):
-                    with air.p():
-                        air.strong(_t=item.title)
-                        air(': ')
-                        date = utc_to_local(item.pub_date)
-                        air(date.strftime("%A, %B %d, %Y %T %z"))
-                        with air.div(klass="itemdesc"):
-                            air(item.description)
-                        with air.div(klass="links", style="background-color: rgba(0,128,64,.25);"):
-                            with air.p():
-                                air.a(href=item.link, _t="Link")
-                            with air.p():
-                                air.strong(_t="Enclosures:")
-                            with air.ol():
-                                for enc in item.enclosures:
-                                    with air.li():
-                                        air.a(href=enc.url, _t="{0} link".format(enc.type))
+                air.h1(_t="Items")
+                for item in feed.items:
+                    with air.div(klass="item", style="border: 1px solid white; margin: 1em; background-color: rgb(231,254,255);"):
+                        with air.p():
+                            air.strong(_t=item.title)
+                            air(': ')
+                            date = utc_to_local(item.pub_date)
+                            air(date.strftime("%A, %B %d, %Y %T %z"))
+                            with air.div(klass="itemdesc"):
+                                air(item.description)
+                            with air.div(klass="links", style="background-color: rgba(0,128,64,.25);"):
+                                with air.p():
+                                    air.a(href=item.link, _t="Link")
+                                with air.p():
+                                    air.strong(_t="Enclosures:")
+                                with air.ol():
+                                    for enc in item.enclosures:
+                                        with air.li():
+                                            air.a(href=enc.url, _t="{0} link".format(enc.type))
     return str(air)
 
 if len(sys.argv) == 1:
